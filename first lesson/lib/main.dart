84e9a26afba52a01abab0a70cfc75e5fef22192b
ahmed wael <ahmedwael99104@gmail.com>
2024-07-29 04:41:33 +0300

    variables part one

---
+++ first lesson/lib/main.dart
@@ -0,0 +1,20 @@
+void main() {
+  // Ahmed changes countinously during the program is runned
+  // I need to represent Ahmed as a variable
+  // Representing as a vaiable makes it easier to change the value
+  print('my name is Ahmed');
+  print('Ahmed enjoys studying programming');
+  print('Ahmed enjoys redaing books');
+  print('my name is Ahmed');
+  print('Ahmed enjoys studying programming');
+  print('Ahmed enjoys redaing books');
+  print('my name is Ahmed');
+  print('Ahmed enjoys studying programming');
+  print('Ahmed enjoys redaing books');
+  print('my name is Ahmed');
+  print('Ahmed enjoys studying programming');
+  print('Ahmed enjoys redaing books');
+  print('my name is Ahmed');
+  print('Ahmed enjoys studying programming');
+  print('Ahmed enjoys redaing books');
+}
